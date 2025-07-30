6c26e9d685e19d21276e7754dfc43b75cc3ca388
evgenyfedoseev <ef7@mail.ru>
2025-07-30 14:25:13 +0300

    initial commit

---
+++ src/test/java/org/example/flexmindtest/FlexmindTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flexmindtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexmindTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
